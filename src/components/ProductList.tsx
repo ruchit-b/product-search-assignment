3983ba42cc906ec08f70ca9c2f6e178dd29e87a9
Ruchit Bhandari <ruchit@leapwallet.io>
2025-11-14 14:04:49 +0530

    fix

--- src/components/ProductList.tsx
+++ src/components/ProductList.tsx
@@ -1,15 +1,16 @@
-import type { Product } from "../hooks/useProducts";
-import ProductItem from "./ProductItem";
+import type { Product } from '../hooks/useProducts'
+import ProductItem from './ProductItem'
 
 function ProductList({ products, loading }: { products: Product[]; loading: boolean }) {
+  if (products.length === 0) {
+    return <div className='no-products-found'>No products found</div>
+  }
   return (
     <div className='product-list'>
       {loading ? (
         <div>Loading...</div>
-      ) : products.length > 0 ? (
-        products.map((product) => <ProductItem key={product.id} product={product} />)
       ) : (
-        <div className='no-products-found'>No products found</div>
+        products.map((product) => <ProductItem key={product.id} product={product} />)
       )}
     </div>
   )
